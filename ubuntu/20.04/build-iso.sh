e3b544033c07d15f6cafaaf02f133b705b0cc99d
tim-oe <tecronin@gmail.com>
2020-12-25 23:31:44 -0600

    added mount and alaises, speed build

--- ubuntu/20.04/build-iso.sh
+++ ubuntu/20.04/build-iso.sh
@@ -6,29 +6,43 @@ set -e
 : "${BIN_XORRISO:=$(type -P xorriso)}"
 : "${BIN_CPIO:=$(type -P gnucpio || type -P cpio)}"
 
+# get directories
+CURRENT_DIR="`pwd`"
+DIST_DIR="$CURRENT_DIR/dist"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # get parameters
-SSH_PUBLIC_KEY_FILE=${1:-"$HOME/.ssh/id_rsa.pub"}
-TARGET_ISO=${2:-"`pwd`/ubuntu-20.04-netboot-amd64-unattended.iso"}
+if [ $1 = "force" ]; then
+    rm -vR "$DIST_DIR"
+    SSH_PUBLIC_KEY_FILE="$HOME/.ssh/id_rsa.pub"
+    TARGET_ISO="`pwd`/ubuntu-20.04-netboot-amd64-unattended.iso"
+else
+    SSH_PUBLIC_KEY_FILE=${1:-"$HOME/.ssh/id_rsa.pub"}
+    TARGET_ISO=${2:-"`pwd`/ubuntu-20.04-netboot-amd64-unattended.iso"}
+fi
 
 # check if ssh key exists
-if [ ! -f "$SSH_PUBLIC_KEY_FILE" ];
-then
+if [ ! -f "$SSH_PUBLIC_KEY_FILE" ]; then
     echo "Error: public SSH key $SSH_PUBLIC_KEY_FILE not found!"
     exit 1
 fi
 
-# get directories
-CURRENT_DIR="`pwd`"
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-TMP_DOWNLOAD_DIR="`mktemp -d`"
+# create working dirs
 TMP_DISC_DIR="`mktemp -d`"
 TMP_INITRD_DIR="`mktemp -d`"
 
 # download and extract netboot iso
 SOURCE_ISO_URL="http://archive.ubuntu.com/ubuntu/dists/focal/main/installer-amd64/current/legacy-images/netboot/mini.iso"
-cd "$TMP_DOWNLOAD_DIR"
-wget -4 "$SOURCE_ISO_URL" -O "./netboot.iso"
-"$BIN_7Z" x "./netboot.iso" "-o$TMP_DISC_DIR"
+
+if [ ! -d "$DIST_DIR" ]; then
+    mkdir -p "$DIST_DIR"
+fi
+
+if [ ! -f "$DIST_DIR/netboot.iso" ]; then
+    wget -4 "$SOURCE_ISO_URL" -O "$DIST_DIR/netboot.iso"
+fi
+
+"$BIN_7Z" x "$DIST_DIR//netboot.iso" "-o$TMP_DISC_DIR"
 
 # patch boot menu
 cd "$TMP_DISC_DIR"
@@ -41,6 +55,13 @@ mkdir "./custom"
 cp "$SCRIPT_DIR/custom/preseed.cfg" "./preseed.cfg"
 cp "$SSH_PUBLIC_KEY_FILE" "./custom/userkey.pub"
 cp "$SCRIPT_DIR/custom/ssh-host-keygen.service" "./custom/ssh-host-keygen.service"
+
+# TEC 
+echo "copying from $SCRIPT_DIR"
+cp "$SCRIPT_DIR/custom/.bash_aliases" "./custom/.bash_aliases"
+dos2unix "./custom/.bash_aliases"
+cp "$SCRIPT_DIR/custom/fstab" "./custom/fstab"
+dos2unix "./custom/fstab"
 
 # append assets to initrd image
 cd "$TMP_INITRD_DIR"
@@ -58,7 +79,6 @@ rm -r '[BOOT]'
 cd "$CURRENT_DIR"
 
 # delete all temporary directories
-rm -r "$TMP_DOWNLOAD_DIR"
 rm -r "$TMP_DISC_DIR"
 rm -r "$TMP_INITRD_DIR"
 
