b33cbd6277bb4b2970e783944eb781155104a361
tim-oe <tecronin@gmail.com>
2020-12-28 02:23:10 -0600

    added snap and more init code

--- ubuntu/20.04/systems/desktop-vm/init.sh
+++ ubuntu/20.04/systems/desktop-vm/init.sh
@@ -1,1 +1,15 @@
-dconf write /org/gnome/shell/favorite-apps "['firefox.desktop','vlc.desktop','org.gnome.Terminal.desktop','org.gnome.Nautilus.desktop','yelp.desktop']"
+# install shotcut
+sudo snap install shotcut --classic
+dconf write /org/gnome/shell/favorite-apps "['firefox.desktop','vlc.desktop','shotcut_shotcut.desktop','org.gnome.Terminal.desktop','org.gnome.Nautilus.desktop','yelp.desktop']"
+
+sudo mkdir -p /mnt/stuff
+
+sudo bash -c 'printf "%s\n" "" "//tec-nas/stuff /mnt/stuff cifs credentials=/home/tcronin/.smbcreds,uid=tcronin,gid=tcronin 0 0" >> /etc/fstab'
+
+read -ep " username: " USER_NAME
+read -ep " password: " PWD
+
+printf "%s\n" "username=$USER_NAME" "password=$PWD" "domain=workgroup" >> .smbcreds
+chmod 600 .smbcreds
+
+sudo mount -a
